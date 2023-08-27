aaa8796fd4fe5724ab8d46cb62ad00bfa2283b77
James <james218.lin@gmail.com>
2023-08-27 05:43:45 -0400

    modified ports

--- app.py
+++ app.py
@@ -85,5 +85,5 @@ def handle_record():
 
     return str(resp)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
